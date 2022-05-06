9426db257104e944b4df5357d56aa5f8038a2e35
Behnam <behnam.bozorgi71@gmail.com>
2022-05-06 10:10:33 +0200

    left overs

---
+++ src/components/Layout/Layout.tsx
@@ -0,0 +1,43 @@
+import React, {FC} from 'react';
+import styles from './Layout.module.css';
+import {Link, Outlet, useLocation, useNavigate} from "react-router-dom";
+import {Layout, Menu} from 'antd';
+import {MenuClickEventHandler} from "rc-menu/lib/interface";
+
+interface LayoutProps {
+}
+
+const {Header, Footer, Content} = Layout;
+
+
+const Baselayout: FC<LayoutProps> = () => {
+    const location = useLocation();
+    return (
+        <div className={styles.Layout} data-testid="Layout">
+            <Layout style={{height: '100%'}}>
+                <Header>
+                    <Menu theme="dark" mode="horizontal" defaultSelectedKeys={[location.pathname]} >
+                        <Menu.Item key="/">
+                            <span>Quiz</span>
+                            <Link to="/"/>
+                        </Menu.Item>
+                        <Menu.Item key="/results/leaderboard">
+                            <span>Leaderboard</span>
+                            <Link to="/results/leaderboard"/>
+                        </Menu.Item>
+                    </Menu>
+                </Header>
+                <Content style={{ padding: '0 50px' }}>
+                    <div className="site-layout-content">
+                        <Outlet/>
+                    </div>
+                </Content>
+                <Footer style={{ textAlign: 'center' }}>
+                    @2022 Behnam. All Rights Reserved
+                </Footer>
+            </Layout>
+        </div>
+    )
+};
+
+export default Baselayout;
